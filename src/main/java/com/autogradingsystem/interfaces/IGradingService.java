41e1f8a58741526666d5e148cb06834eac1d4b73
jiaqianwan2024 <jiaqian.wan.2024@smu.edu.sg>
2026-02-03 13:52:52 +0800

    File structure setup

--- src/main/java/com/autogradingsystem/interfaces/IGradingService.java
+++
@@ -1,8 +0,0 @@
-package com.autogradingsystem.interfaces;
-
-import com.autogradingsystem.model.Student;
-import java.io.File;
-
-public interface IGradingService {
-    void gradeStudent(Student student, File unzippedFolder);
-}
