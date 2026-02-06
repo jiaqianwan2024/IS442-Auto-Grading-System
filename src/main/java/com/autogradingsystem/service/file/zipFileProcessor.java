dd69b574f2a0f4f39dab45a97bd3acdf0f0d9e5d
Matthewkhlr <matthewkhoolr@gmail.com>
2026-02-06 10:36:19 +0800

    Feat: Added ZipFileProcessor, which takes in a filepath and a directory and unzips a single file at the filepath and sends it to the directory

--- src/main/java/com/autogradingsystem/service/file/zipFileProcessor.java
+++ src/main/java/com/autogradingsystem/service/file/zipFileProcessor.java
@@ -1,12 +1,39 @@
-package com.autogradingsystem.service.file;
+
 
 import java.io.*;
 import java.nio.file.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-public class zipFileProcessor{
-    public static void unzip (String zipFilePath, String destDirectory){
-        
+public class ZipFileProcessor{
+    public static void unzip (String zipFilePath, String destDirectory) throws IOException{
+        File destDir = new File(destDirectory); //Java object pointing to a file path
+        if (!destDir.exists()) { 
+            destDir.mkdirs(); // Create the folder if it doesn't exist
+        }
+
+        try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath))) {
+            ZipEntry entry = zipIn.getNextEntry(); //Read the next entry in the file 
+
+            while (entry != null){ //Iterate through each file in the zip file
+                Path studentFilePath = Paths.get(destDirectory, entry.getName()); // Paths.get makes a new file inside the file path specified at function call
+
+                if (!entry.isDirectory()){
+                    Files.createDirectories(studentFilePath.getParent()); //Check for parent directory, create if it does not exist
+                    //Since the parents on the filepath may not exist, this is impt
+                    
+                    Files.copy(zipIn, studentFilePath, StandardCopyOption.REPLACE_EXISTING);
+                    //Copy code in the file into the studentFilePath
+                } else {
+                    Files.createDirectories(studentFilePath);
+                }
+                zipIn.closeEntry(); 
+                entry = zipIn.getNextEntry(); //Move onto the next file inside the zipfile
+
+
+            }
+
+            
+    }
     }
 }
